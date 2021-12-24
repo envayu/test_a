7bdda99ac1e7fa2e6531170dd7f63bb79bae93fc
envayu <envayu00@gmail.com>
2021-12-24 10:39:21 +0900

    BoardDAO, GoodsDAO

---
+++ test_a/src/main/java/com/sist/dao/BoardDAO.java
@@ -0,0 +1,11 @@
+package com.sist.dao;
+
+public class BoardDAO {
+	public int insert() {
+		System.out.println("게시물을 등록 하였습니다.");
+		return 1;
+	}
+	public void delete() {
+		System.out.println("게시물을 삭제 하였습니다.");
+	}
+}
